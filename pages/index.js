79b34e39aa9c54b9da707e0503658daaee03b63a
Godofredo C Gatus II <hit1st4731@sbcglobal.net>
2021-06-05 09:53:33 -0700

    react redux

--- pages/index.js
+++ pages/index.js
@@ -3,20 +3,58 @@
 // const 
 
 // export default Rejection;
-
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 import { reducer, addQuestion, getScore } from '../src/features/rejection/rejection-reducer.js';
-import { storeQuestions, getStoredQuestions } from '../src/features/stored-questions/stored-questions.js';
-import getFormValues from '../src/features/form-values/form-values.js';
+// import { storeQuestions, getStoredQuestions } from '../src/features/stored-questions/stored-questions.js';
+// import getFormValues from '../src/features/form-values/form-values.js';
 
-const processInput = () => {
-  const prevState = getStoredQuestions() || reducer();
-  const state = reducer(prevState, addQuestion(getFormValues()));
-  storeQuestions(state);
+// const processInput = () => {
+//   const prevState = getStoredQuestions() || reducer();
+//   const state = reducer(prevState, addQuestion(getFormValues()));
+//   storeQuestions(state);
 
-  const myStorage = window.localStorage;
-  console.log('new store: ', JSON.parse(myStorage.getItem('questions')))
+//   const myStorage = window.localStorage;
+//   console.log('new store: ', JSON.parse(myStorage.getItem('questions')))
+// };
+
+// window.processInput = processInput;
+
+// export default processInput;
+
+const store = createStore(reducer);
+
+const Rejections = () => {
+  const accepted = () => store.dispatch(addQuestion({
+    question: 'Did it work?',
+    askee: 'V8',
+    status: 'Accepted'
+  }));
+  const rejected = () => store.dispatch(addQuestion({
+    question: 'Did it work?',
+    askee: 'V8',
+    status: 'Rejected'
+  }));
+
+  return (
+    <>
+      <h1>Score: {getScore(store.getState())}</h1>
+      <ul>
+        {store.getState().map(rejection => 
+          <li key={rejection.id}>
+            {JSON.stringify(rejection)}
+          </li>
+        )}
+      </ul>
+      <button onClick={accepted}>Accepted</button>
+      <button onClick={rejected}>Rejected</button>
+    </>
+  );
+};
+const render = () => {
+  ReactDOM.render(<Rejections />, document.getElementById('root'));
 };
 
-window.processInput = processInput;
-
-export default processInput;
+store.subscribe(render);
+render();
