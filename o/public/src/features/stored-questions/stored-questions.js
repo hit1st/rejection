05757e752cd69f2afd08e1dbbae7ad31ccd26c51
Godofredo C Gatus II <hit1st4731@sbcglobal.net>
2021-06-20 10:56:32 -0700

    deleted unused files

--- o/public/src/features/stored-questions/stored-questions.js
+++
@@ -1,7 +0,0 @@
-const myStorage = window.localStorage;
-
-const storeQuestions = questions => myStorage.setItem('questions', JSON.stringify(questions));
-
-const getStoredQuestions = () => JSON.parse(myStorage.getItem('questions'));
-
-export { storeQuestions, getStoredQuestions };
