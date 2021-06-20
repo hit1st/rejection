1fd46ca60106b1652927024b33ab5027377fbae8
Godofredo C Gatus II <hit1st4731@sbcglobal.net>
2021-06-19 23:13:52 -0700

    save state to local storage

---
+++ src/features/local-storage/localStorage.js
@@ -0,0 +1,20 @@
+const loadState = () => {
+  try {
+    const serializedState = localStorage.getItem('state');
+    if (serializedState === null) return;
+    return JSON.parse(serializedState);
+  } catch (err) {
+    return;
+  }
+};
+
+const saveState = state => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem('state', serializedState);
+  } catch (err) {
+    // ignore for now
+  }
+}
+
+export { loadState, saveState };
--- src/features/rejections-app/store.js
+++ src/features/rejections-app/store.js
@@ -1,12 +1,24 @@
 import { createStore, combineReducers } from 'redux';
 import { reducer } from '../rejection/rejection-reducer.js';
 import visibilityFilter from '../visibility/visibility-filter.js';
+import { loadState, saveState } from '../local-storage/localStorage';
 
 const rejectionsApp = combineReducers({
   rejections: reducer,
   visibilityFilter
 });
 
-const store = createStore(rejectionsApp);
+const persistedState = loadState();
+
+const store = createStore(
+  rejectionsApp,
+  persistedState
+);
+
+store.subscribe(() => {
+  saveState({
+    rejections: store.getState().rejections
+  });
+});
 
 export default store;
