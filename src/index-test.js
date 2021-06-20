3ee6e06dcc2cdcd9410d1f743812f1ac1195901f
Godofredo C Gatus II <hit1st4731@sbcglobal.net>
2021-06-19 18:45:22 -0700

    added action creators

--- src/index-test.js
+++ src/index-test.js
@@ -1,2 +1,3 @@
 import './features/rejection/rejection-reducer-test.js';
+import './features/action-creators/action-creators-test.js';
 // import './features/get-stored-questions/get-stored-questions-test.js';
