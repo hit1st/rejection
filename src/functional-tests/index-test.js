aa6184cfe6efa64a792f29a87a4cea056c642605
Godofredo C Gatus II <hit1st4731@sbcglobal.net>
2021-06-22 09:50:10 -0700

    first functional tests

---
+++ src/functional-tests/index-test.js
@@ -0,0 +1,16 @@
+import { Selector } from 'testcafe';
+
+fixture`Rejection Homepage`
+  .page('http://localhost:8081');
+
+test('Page should load and display the correct title.', async t => {
+  const actual = Selector('h1').innerText;
+  const expected = 'Rejections';
+  await t.expect(actual).eql(expected);
+});
+
+test('Page should load and display the score.', async t => {
+  const actual = Selector('h3').innerText;
+  const expected = 'Score: 0';
+  await t.expect(actual).eql(expected);
+});
