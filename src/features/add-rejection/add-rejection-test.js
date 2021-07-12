98208f259bd61728c791fe6043f50bb7661dd902
Godofredo C Gatus II <hit1st4731@sbcglobal.net>
2021-07-12 08:19:27 -0700

    Input tests

--- src/features/add-rejection/add-rejection-test.js
+++ src/features/add-rejection/add-rejection-test.js
@@ -3,7 +3,88 @@ import { describe } from 'riteway';
 import render from 'riteway/render-component';
 import match from 'riteway/match';
 
-import { Input } from './input.js';
+import 
+  Input,
+  {
+    inputReducer,
+    updateInput,
+    clearInput
+  } from './input.js';
+
+describe('input/updateInput', async assert => {
+  {
+    const expected = {
+      type: 'ADD_REJECTION::UPDATE_INPUT',
+      payload: ''
+    };
+
+    assert({
+      given: 'no arguments',
+      should: 'return an action object',
+      actual: updateInput(),
+      expected
+    });
+  }
+
+  {
+    const value = 'Can I have a raise?';
+    const expected = {
+      type: 'ADD_REJECTION::UPDATE_INPUT',
+      payload: value
+    };
+
+    assert({
+      given: 'a new input',
+      should: 'return an action object',
+      actual: updateInput(value),
+      expected
+    });
+  }
+});
+
+describe('input/clearInput', async assert => {
+  assert({
+    given: 'no arguments',
+    should: 'return action object',
+    actual: clearInput()(),
+    expected: {
+      name: undefined,
+      type: 'ADD_REJECTION::CLEAR_INPUT'
+    }
+  })
+});
+
+describe('inputReducer', async assert => {
+  assert({
+    given: 'no arguments',
+    should: 'return a valid initial state',
+    actual: inputReducer(),
+    expected: ''
+  });
+
+  {
+    const value = 'Can I have a raise?';
+
+    assert({
+      given: 'a new value',
+      should: 'update state',
+      actual: inputReducer(inputReducer(), updateInput(value)),
+      expected: value
+    });
+  }
+
+  {
+    const state = 'not empty string';
+
+    assert({
+      given: 'state and clearInput action',
+      should: 'return an empty string',
+      actual: inputReducer(state, clearInput()()),
+      expected: ''
+    });
+  }
+});
+
 
 describe('Input component', async assert => {
   const createInput = ({ 
@@ -23,34 +104,69 @@ describe('Input component', async assert => {
 
     assert({
       given: 'no arguments',
-      should: 'render no header.',
-      actual: $('h3').html(),
-      expected: null
-    });
-  }
-
-  {
-    const headerValue = 'Name';
-    const $ = createInput({ headerValue, inputValue: 'str' });
-    const contains = match($('.input-header').html());
-
-    assert({
-      given: 'a header value',
-      should: 'render the correct header text.',
-      actual: contains(headerValue),
-      expected: headerValue
+      should: 'render nothing.',
+      actual: $('body').html(),
+      expected: ''
     });
   }
 
   {
     const inputValue = 'str';
-    const $ = createInput({ inputValue });
+    const onChangeHandler = () => {};
+    const $ = createInput({ inputValue, onChangeHandler });
+
+    assert({
+      given: 'no headerValue argument',
+      should: 'render nothing.',
+      actual: $('body').html(),
+      expected: ''
+    });
+  }
+
+  {
+    const headerValue = 'Name';
+    const inputValue = 'str';
+    const $ = createInput({ headerValue, inputValue });
+
+    assert({
+      given: 'no onChangeHandler argument',
+      should: 'render nothing.',
+      actual: $('body').html(),
+      expected: ''
+    });
+  }
+
+  {
+    const values = {
+      headerValue: 'Name',
+      inputValue: 'str',
+      onChangeHandler: () => {}
+    };
+    const $ = createInput(values);
+    const contains = match($('.input-header').html());
+
+    assert({
+      given: 'a header argument',
+      should: 'render the correct header text.',
+      actual: contains(values.headerValue),
+      expected: values.headerValue
+    });
+  }
+
+  {
+    const values = {
+      headerValue: 'Name',
+      inputValue: 'str',
+      onChangeHandler: () => {}
+    };
+    const $ = createInput(values);
 
     assert({
       given: 'an input value',
       should: 'render the correct input text.',
       actual: $('input').val(),
-      expected: inputValue
+      expected: values.inputValue
     });
   }
+
 });
