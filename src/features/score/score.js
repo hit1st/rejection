bfcbb436f341d7f1d35e14e117ce8161643dc7b6
Godofredo C Gatus II <hit1st4731@sbcglobal.net>
2021-07-12 20:03:23 -0700

    refactored for readability

--- src/features/score/score.js
+++ src/features/score/score.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { getScore } from '../rejection/rejection-reducer.js';
+import { getScore, getRejections } from '../rejection/rejection-reducer.js';
 
 const DisplayScore = ({ score }) => (<h3>Score: {score}</h3>)
 
 const Score = () => {
-  const { rejections } = useSelector(state => state);
+  const rejections = useSelector(getRejections);
 
   return <DisplayScore score={getScore(rejections)} />;
 };
