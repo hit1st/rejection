045066af6dd55a18f4465a021a5e20fba2e04213
Godofredo C Gatus II <hit1st4731@sbcglobal.net>
2021-07-20 09:45:00 -0700

    refactored to use createHeadingFunction to create DisplayScore Component

--- src/features/score/score.js
+++ src/features/score/score.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+
+import createHeadingComponent from '../create-heading-component/create-heading-component.js';
 import { getScore, getRejections } from '../rejection/rejection-reducer.js';
 
-const DisplayScore = ({ score }) => (<h3>Score: {score}</h3>)
+const DisplayScore = createHeadingComponent('h3')('Score');
 
 const Score = () => {
   const rejections = useSelector(getRejections);
 
-  return <DisplayScore score={getScore(rejections)} />;
+  return <DisplayScore value={getScore(rejections)} />;
 };
 
 export default Score;
