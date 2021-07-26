d5dc59bd952e34156ee2e4036c0bcc55fabf79ca
Godofredo C Gatus II <hit1st4731@sbcglobal.net>
2021-07-26 11:43:39 -0700

    added environment variables for faunadb secret and endpoint

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_FAUNADB_SECRET: 'fnAEOyBldYACRKBRlt0woSw_zn4EWNakLDIljxXS',
+    NEXT_PUBLIC_FAUNADB_GRAPHQL_ENDPOINT: 'https://graphql.fauna.com/graphql'
+  }
 }
