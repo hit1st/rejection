d37db9cc87d28128719c80fb66ab7a0c23d0e9c5
Godofredo C Gatus II <hit1st4731@sbcglobal.net>
2021-07-28 13:41:58 -0700

    removed unused reducer and associated tests

--- src/features/input/input.js
+++ src/features/input/input.js
@@ -17,30 +17,4 @@ const Input = (values = {
   onChangeHandler: () => {}
 }) => displayView(values);
 
-const updateInput = (value = '') => ({
-  type: 'ADD_REJECTION::UPDATE_INPUT',
-  payload: value
-});
-
-const clearInput = (name) => () => ({
-  name,
-  type: 'ADD_REJECTION::CLEAR_INPUT'
-});
-
-const inputReducer = (
-  state = '',
-  { type, payload } = {}
-) => {
-  switch (type) {
-    case 'ADD_REJECTION::UPDATE_INPUT':
-      return payload
-    case 'ADD_REJECTION::CLEAR_INPUT':
-      return ''
-    default:
-      return state;
-  }
-};
-
 export default Input;
-
-export { inputReducer, updateInput, clearInput };
