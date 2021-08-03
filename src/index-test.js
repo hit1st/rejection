d403d869d493805e2735db6d2f182d7bdc3b73df
Godofredo C Gatus II <hit1st4731@sbcglobal.net>
2021-08-02 18:59:36 -0700

    removed unused test suite

--- src/index-test.js
+++ src/index-test.js
@@ -2,7 +2,6 @@ import './features/rejection/rejection-reducer-test.js';
 import './features/visibility/visibility-filter-test.js';
 import './features/id-reducer/id-reducer-test.js';
 import './features/faunadb-sagas/faunadb-sagas-test.js';
-// import './features/local-storage-sagas/local-storage-sagas-test.js';
 import './features/input/input-test.js';
 import './features/button/button-test.js';
 import './features/add-rejection/add-rejection-test.js';
