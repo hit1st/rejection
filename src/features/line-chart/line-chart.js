37539e57241c865137f6dd4f3dc5f6df608db2cc
Godofredo C Gatus II <hit1st4731@sbcglobal.net>
2021-08-13 15:18:43 -0700

    display component for line chart

--- src/features/line-chart/line-chart.js
+++ src/features/line-chart/line-chart.js
@@ -1,19 +1,92 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { useSelector } from 'react-redux';
-import { getRejections } from '../rejection/rejection-reducer.js';
-import { dateMaker, daysForTheWeek, getDailyScoreForTheDuration } from '../../utils/date-utils.js';
+import {
+  timeParse,
+  scaleTime,
+  scaleLinear,
+  extent,
+  max,
+  axisBottom,
+  axisLeft,
+  timeFormat,
+  line,
+  select
+} from 'd3';
 
-const LineChart = () => {
-  const rejections = useSelector(getRejections);
-  console.log('LineChart rejections: ', rejections);
-  const week = daysForTheWeek().map(date => dateMaker(date));
-  const dailyScoreForTheWeek = getDailyScoreForTheDuration(rejections, week);
 
-  console.log('dailyScoreForTheWeek: ', dailyScoreForTheWeek);
+const dateConverter = timeParse("%Y-%m-%d");
+
+const LineChart = ({ rawData = [], dimensions = {} }) => {
+  const svgRef = useRef(null);
+  const data = rawData.map(({ date, score }) => ({ date: dateConverter(date), score }));
+  const {
+    width = 960,
+    height = 520,
+    margin = {top: 20, right: 60, bottom: 60, left: 40}
+  } = dimensions;
+
+  console.log('data: ', data);
+  // Step 3. Create x and y axes.
+  const xScale = scaleTime()
+    .domain(extent(data, d => d.date))
+    .range([margin.left, width - margin.right]);
+
+  const yScale = scaleLinear()
+    .domain([0, max(data, (d) => d.score)]).nice()
+    .range([height - margin.bottom, margin.top]);
+
+  // Create root container where we will append all other chart elements
+  const svgEl = select(svgRef.current);
+  // Clear svg content before adding new elements
+  svgEl.selectAll('*').remove();
+  const svg = svgEl
+    .append('g')
+    .attr("transform", `translate(${margin.left},${margin.top})`);
+
+  const xAxis = (g) => g
+    .attr('transform', `translate(0,${height - margin.bottom})`)
+    .call(axisBottom(xScale).tickFormat(timeFormat("%b %d")).ticks(width / 115).tickSizeOuter(0));
+
+  const yAxis = (g) => g
+    .attr('transform', `translate(${margin.left},0)`)
+    .call(axisLeft(yScale));
+
+
+    // Step 4. Define the line function.
+  const linePath = line()
+    .defined(d => !isNaN(d.score))
+    .x(d => xScale(d.date))
+    .y(d => yScale(d.score));
+
+  svg.append('g').call(xAxis);
+  svg.append('text')
+    .attr('x', width / 2)
+    .attr('y', height - 25)
+    .style('text-anchor', 'middle')
+    .attr('font-family', 'sans-serif')
+    .text('Days of the Week');
+
+  svg.append('g').call(yAxis);
+  svg.append('text')
+    .attr('transform', `rotate(-90,15,${height / 2})`)
+    .attr('x', 15)
+    .attr('y', height / 2)
+    .style('text-anchor', 'middle')
+    .attr('font-family', 'sans-serif')
+    .text('Score');
+
+  // Draw the line.
+  svg.append('path')
+    .datum(data)
+    .attr('d', linePath)
+    .style('fill', 'none')
+    .style('stroke', 'steelblue')
+    .style('stroke-width', '1.50')
+    .style('stroke-linejoin', 'round')
+    .style('stroke-linecap', 'round');
 
   return (
-    <>
-    </> 
+    <svg ref={svgRef} width={width} height={height} /> 
   )
 };
 
