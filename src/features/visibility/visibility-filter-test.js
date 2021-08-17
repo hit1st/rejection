e7117f0a3d6a0aae862fadb4ab35d0c6c0ad1be4
Godofredo C Gatus II <hit1st4731@sbcglobal.net>
2021-08-17 15:51:54 -0700

    refactored combining multiple unit tests into fewer tests

--- src/features/visibility/visibility-filter-test.js
+++ src/features/visibility/visibility-filter-test.js
@@ -1,79 +1,25 @@
 import { describe } from 'riteway';
 import visibilityFilter, { getVisibilityFilter, setVisibilityFilter } from './visibility-filter.js';
 
-describe('getVisibilityFilter', async assert => {
-  assert({
-    given: 'no arguments',
-    should: 'return undefined',
-    actual: getVisibilityFilter(),
-    expected: undefined
-  });
-
-  {
-    const state = {}
-
-    assert({
-      given: 'invalid state',
-      should: 'return undefined',
-      actual: getVisibilityFilter(state),
-      expected: undefined
-    });
-  }
-
-  {
-    const state = {
-      visibilityFilter: 'SHOW_ALL'
-    }
-
-    assert({
-      given: 'valid state',
-      should: 'return valid visibilityFilter value',
-      actual: getVisibilityFilter(state),
-      expected: state.visibilityFilter
-    });
-  }
-});
-
-describe('setVisibilityFilter', async assert => {
-  assert({
-    given: 'no arguments',
-    should: 'return undefined',
-    actual: setVisibilityFilter(),
-    expected: undefined
-  });
-
-  { 
-    const filter = 'SHOW_ALL';
-    const expected = {
-      type: 'SET_VISIBILITY_FILTER',
-      filter
-    };
-
-    assert({
-      given: 'an argument',
-      should: 'return action object',
-      actual: setVisibilityFilter(filter),
-      expected
-    });
-  }
-});
-
-describe('visibilityFilter', async assert => {
+describe('getVisibilityFilter/visibilityFilter', async assert => {
   assert({
     given: 'no arguments',
     should: 'return a valid initial state',
-    actual: visibilityFilter(),
+    actual: getVisibilityFilter({ visibilityFilter: visibilityFilter() }),
     expected: 'SHOW_ALL'
   });
+});
 
-  {
-    const filter = 'SHOW_ACCEPTED';;
+describe('getVisibilityFilter/visibilityFilter/setVisibilityFilter', async assert => {
+  const filter = 'SHOW_ACCEPTED';
+  const actual = getVisibilityFilter({
+    visibilityFilter: visibilityFilter(visibilityFilter(), setVisibilityFilter(filter))
+  });
 
-    assert({
-      given: 'a state and action creator',
-      should: 'change state',
-      actual: visibilityFilter(visibilityFilter(), setVisibilityFilter(filter)),
-      expected: filter
-    });
-  }
+  assert({
+    given: 'a state and action creator',
+    should: 'change state',
+    actual,
+    expected: filter
+  });
 });
