e772c043c53379203b45d2e9466337f320e12709
Godofredo C Gatus II <hit1st4731@sbcglobal.net>
2021-08-19 11:53:07 -0700

    increased margin

--- src/features/button/button.js
+++ src/features/button/button.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Button = ({ buttonClass, label, handleClick }) => (
-  <button className={buttonClass} onClick={handleClick}>{label}</button>
+  <button className={buttonClass} onClick={handleClick} style={{ margin: 10 }}>{label}</button>
 );
 
 export default Button;
