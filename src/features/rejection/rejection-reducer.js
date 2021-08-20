1c8eaf4042d562336de77b386428454a5c805729
Godofredo C Gatus II <hit1st4731@sbcglobal.net>
2021-08-20 14:50:49 -0700

    refactored getScore

--- src/features/rejection/rejection-reducer.js
+++ src/features/rejection/rejection-reducer.js
@@ -27,7 +27,7 @@ const addFetchedQuestions = (fetchedQuestions = []) => {
 };
 
 const getScore = state =>
-  state.rejections.reduce((score, question) => (
+  getRejections(state).reduce((score, question) => (
     question.status === 'Accepted' ? score + 1 :
     question.status === 'Rejected' ? score + 10 :
     score
